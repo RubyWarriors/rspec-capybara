385eb20b228fae8de72d67ee63f84328d708fe73
Rafkat <ufadiz@gmail.com>
2015-11-01 23:00:46 +0500

    Adding article validation

--- Gemfile
+++ Gemfile
@@ -40,21 +40,20 @@ group :development, :test do
   gem 'spring-commands-rspec'
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug'
-  
- 
+
+
 end
 
 group :development do
   # Access an IRB console on exception pages or by using <%= console %> in views
   gem 'web-console', '~> 2.0'
-
+  gem 'terminal-notifier-guard'
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
 end
 
-group :test do 
+group :test do
   #capybara
   gem 'capybara'
-	
-end
 
+end
--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,22 +1,26 @@
 class ArticlesController < ApplicationController
-	
+
   def index
   end
 
   def new
   	@article = Article.new
-  end	
+  end
 
   def create
   	@article = Article.new(article_params)
-  	@article.save
-  	flash[:success] = "Article has been created"
-  	redirect_to articles_path
-  end	
+  	if @article.save
+  		flash[:success] = "Article has been created"
+  		redirect_to articles_path
+		else
+			flash[:danger] = "Article has not been created"
+			render :new
+		end	
+  end
 
-  private 
+  private
   	def article_params
   	  params.require(:article).permit(:title, :body)
-  	end	
+  	end
 
 end
